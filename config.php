5ffce458d652b20ecdc85d76e661a07d6452be54
Handryansyah Purba <102147380+handryanp@users.noreply.github.com>
2025-10-16 01:41:03 +0700

    Delete config.php

--- config.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$base_url = "http://localhost/kvlar-portofolio/";
-$OPENAI_API_KEY = "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";
-?>
